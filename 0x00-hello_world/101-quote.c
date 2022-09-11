e4b79d99f1b38bfe5a032d7fe7b91dfbad7e5781
Fawemimo <syllabustech01@gmail.com>
2022-09-11 01:38:59 -0700

    unix simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Output the context given
+ *
+ * Return: Always  return 1
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\"-Dora Korpar, 2015-10-19\n",55);
+	return (1);
+}
